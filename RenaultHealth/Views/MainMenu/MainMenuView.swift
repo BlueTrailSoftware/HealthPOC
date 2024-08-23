969e1db3fae49c23e4d2a78d315a2360b79bfa88
Manu Salinas <manuels@bluetrailsoft.com>
2024-08-23 13:22:59 -0600

    MainMenuView update

--- RenaultHealth/Views/MainMenu/MainMenuView.swift
+++ RenaultHealth/Views/MainMenu/MainMenuView.swift
@@ -8,28 +8,40 @@
 import SwiftUI
 
 struct MainMenuView: View {
+    private let menuItems = ["Apple HealthKit\nSleep Data", "Light Formula\nCalculation"]
+
     var body: some View {
-        NavigationView(content: {
-            
-            VStack {
-                
-                NavigationLink(destination: HealthDataView()
-                ) { Text("Healthkit - User sleep data") }
-                    .padding(.horizontal, 16)
-                    .frame(height: 44)
-                    .background(.purple)
-                    .cornerRadius(8)
-                    .foregroundColor(.white)
-                
-                NavigationLink(destination: LightFormulaParametrizedTestingView()
-                ) { Text("Light Formula calculation") }
-                    .padding(.horizontal, 16)
-                    .frame(height: 44)
-                    .background(.blue)
-                    .cornerRadius(8)
-                    .foregroundColor(.white)
+        NavigationStack {
+            GeometryReader { geometry in
+                LazyVStack(spacing: 0) {
+                    ForEach(menuItems.enumerated().map({ $0 }), id: \.element) { index, item in
+                        NavigationLink {
+                            switch index {
+                            case 0:
+                                HealthDataView()
+                            default:
+                                LightFormulaParametrizedTestingView()
+                            }
+                        } label: {
+                            Text(item)
+                                .padding()
+                                .font(.largeTitle)
+                                .fontWeight(.medium)
+                                .frame(width: geometry.size.width, height: geometry.size.height / 2)
+                                .foregroundColor(.white)
+                                .background (
+                                    LinearGradient(gradient: Gradient(colors: [
+                                        index == 0 ? .vibrantPurple : .blue,
+                                        index == 0 ? .vibrantPurple.opacity(0.9) : .blue.opacity(0.8) ,
+                                        index == 0 ? .deepPurple : .blue
+                                    ]), startPoint: .top, endPoint: .center)
+                                )
+
+                        }
+                    }
+                }
             }
-        })
+        }
     }
 }
 
